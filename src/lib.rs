987b1eaa1b574d53ef061693c1160de97ecb2ed5
Babis Chalios <mail@bchalios.io>
2023-05-01 11:42:36 +0200

    Add supported Firecracker version
    Add in the root crate info about which Firecracker version we support.
    Expose a function that can be used to query the supported version.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,11 @@ pub mod instance;
 pub mod kernel;
 
 pub use instance::{FcVmm, FcVmmBuilder, LogLevel};
+
+use semver::Version;
+
+static FC_VERSION: &str = "1.3.0";
+
+pub fn supported_fc_version() -> Version {
+    Version::parse(FC_VERSION).unwrap()
+}
